ea34f329cfee1998383eac6c16f1ff6c9535281c
Bakary007 <bakary.traore@modernsolutions.cc>
2023-12-21 21:46:40 +0100

    controller update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -10,10 +10,14 @@ const numbers = (jsonData.response.changes)
 
 const getBookingsByDateRange = (req, res) => {
     const { startDate, endDate } = req.query;
-    console.log(req.query);
 
     const sql = `
-        SELECT bh.*, be.*
+        SELECT bh.id AS booking_id, bh.number, bh.trip_name, bh.status_code, bh.status_name,
+               bh.company_name, bh.deptor_place, bh.contact_first_name, bh.contact_middle_name,
+               bh.contact_surname, bh.summary, bh.startdate AS booking_startdate, bh.enddate AS booking_enddate,
+               be.id AS element_id, be.element_name, be.element_type_code, be.supplier_place,
+               be.supplier_country, be.startdate AS element_startdate, be.starttime,
+               be.enddate AS element_enddate, be.endtime, be.amount, be.amount_description
         FROM bookingheader bh
         LEFT JOIN bookingelement be ON bh.id = be.booking_id
         WHERE bh.startdate >= ? AND bh.enddate <= ?
@@ -27,11 +31,39 @@ const getBookingsByDateRange = (req, res) => {
         const bookings = results.reduce((acc, current) => {
             const bookingId = current.booking_id;
             const booking = acc[bookingId] || {
-                ...current,
+                id: bookingId,
+                number: current.number,
+                trip_name: current.trip_name,
+                status_code: current.status_code,
+                status_name: current.status_name,
+                company_name: current.company_name,
+                deptor_place: current.deptor_place,
+                contact_first_name: current.contact_first_name,
+                contact_middle_name: current.contact_middle_name,
+                contact_surname: current.contact_surname,
+                summary: current.summary,
+                startdate: current.booking_startdate,
+                enddate: current.booking_enddate,
                 booking_elements: []
             };
 
-            booking.booking_elements.push(current);
+            if (current.element_id) {
+                const bookingElement = {
+                    id: current.element_id,
+                    element_name: current.element_name,
+                    element_type_code: current.element_type_code,
+                    supplier_place: current.supplier_place,
+                    supplier_country: current.supplier_country,
+                    startdate: current.element_startdate,
+                    starttime: current.starttime,
+                    enddate: current.element_enddate,
+                    endtime: current.endtime,
+                    amount: current.amount,
+                    amount_description: current.amount_description
+                };
+
+                booking.booking_elements.push(bookingElement);
+            }
 
             acc[bookingId] = booking;
             return acc;
@@ -40,6 +72,7 @@ const getBookingsByDateRange = (req, res) => {
         res.json(Object.values(bookings));
     });
 };
+
 
 const getAllBookings = (req, res) => {
     const sql = `SELECT * FROM bookingheader`;
